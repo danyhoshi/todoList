d1b5db8248234790ab5dd1a7cd48db9456137e89
dgonzalez777 <1989.daniela@gmail.com>
2023-05-23 13:09:42 -0400

    Add remove function

--- src/index.js
+++ src/index.js
@@ -12,6 +12,8 @@ const img = document.querySelectorAll('.container-svg img');
 
 const clearAll = document.querySelector('.clearAll');
 
+const button = document.querySelector('button');
+
 function printTask(element, index) {
   const div = document.createElement('div');
   const divC = document.createElement('div');
@@ -39,6 +41,7 @@ function printTask(element, index) {
 }
 
 function printTasks(tasks) {
+  // console.log('What are you doing?');
   tasks.forEach((element, index) => {
     const div = document.createElement('div');
     const divC = document.createElement('div');
@@ -50,6 +53,7 @@ function printTasks(tasks) {
     inputCheck.id = `task${index}`;
     inputCheck.type = 'checkbox';
     inputCheck.name = `task${index}`;
+    inputCheck.check = element.state;
     inputCheck.checked = element.state;
     inputTask.value = element.description;
     inputTask.setAttribute('name', `ttask${index}`);
@@ -66,6 +70,54 @@ function printTasks(tasks) {
   });
 }
 
+function printTasksFrom(tasks, idR) {
+  // console.log('What are you doing?');
+  tasks.forEach((element, index) => {
+    if (index >= idR) {
+      const div = document.createElement('div');
+      const divC = document.createElement('div');
+      const inputCheck = document.createElement('input');
+      const inputTask = document.createElement('input');
+      const divSvg = document.createElement('div');
+      const svg = document.createElement('img');
+      divC.classList.add('checkbox');
+      inputCheck.id = `task${index}`;
+      inputCheck.type = 'checkbox';
+      inputCheck.name = `task${index}`;
+      inputCheck.check = element.state;
+      inputCheck.checked = element.state;
+      inputTask.value = element.description;
+      inputTask.setAttribute('name', `ttask${index}`);
+      inputTask.classList.add('inTask');
+      divSvg.classList.add('container-svg');
+      svg.setAttribute('src', moreVert);
+      svg.setAttribute('alt', 'edit');
+      divC.appendChild(inputCheck);
+      divC.appendChild(inputTask);
+      div.appendChild(divC);
+      divSvg.appendChild(svg);
+      div.appendChild(divSvg);
+      clearAll.insertAdjacentElement('beforebegin', div);
+    // console.log('What are you doing?');
+    }
+  });
+}
+
+function clear(indexR) {
+  document.querySelectorAll('.containerTodo > div').forEach((element, index) => {
+    if (index > 1 + indexR) {
+      element.remove();
+    }
+  });
+}
+function clearSelected() {
+  document.querySelectorAll('.containerTodo > div').forEach((element, index) => {
+    if (index > 1) {
+      element.remove();
+    }
+  });
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   if (localStorage.getItem('tasks')) {
     tdL.tasks = JSON.parse(localStorage.getItem('tasks'));
@@ -74,9 +126,9 @@ document.addEventListener('DOMContentLoaded', () => {
     });
     printTasks(tdL.getTasks());
   } else {
-    tdL.addTask(new Task('Wash my dog...'));
-    tdL.addTask(new Task('Complete To Do List Project'));
-    tdL.addTask(new Task('Fix car '));
+    tdL.addTask(new Task('Wash my dog...', false));
+    tdL.addTask(new Task('Complete To Do List Project', false));
+    tdL.addTask(new Task('Fix car ', false));
     localStorage.setItem('tasks', JSON.stringify(tdL.getTasks()));
     console.log(localStorage.getItem('tasks'));
     console.log(`GetTask${tdL.getTasks().length}`);
@@ -97,38 +149,91 @@ document.addEventListener('change', (e) => {
     const tskD = document.querySelector(`input[name='t${check.name}']`);
     if (check.checked) {
       tskD.classList.add('checked');
-      console.log(`Task selected: ${tskD.value}`);
+      const idTask = parseInt(check.name.slice(4), 10);
+      tdL.editStatus(idTask, check.checked);
+      localStorage.setItem('tasks', JSON.stringify(tdL.getTasks()));
     } else {
       tskD.classList.remove('checked');
+      const idTask = parseInt(check.name.slice(4), 10);
+      tdL.editStatus(idTask, check.checked);
+      localStorage.setItem('tasks', JSON.stringify(tdL.getTasks()));
     }
+  } else {
+    return false;
   }
+  return true;
+});
+
+document.addEventListener('click', (e) => {
+  const taskR = e.target;
+  if (e.target.matches('img[alt=\'delete\']')) {
+    const tskD = taskR.parentElement.previousElementSibling.firstElementChild.id;
+    const idTask = parseInt(tskD.slice(4), 10);
+    tdL.removeTask(idTask);
+    localStorage.setItem('tasks', JSON.stringify(tdL.getTasks()));
+    clear(idTask);
+    printTasksFrom(tdL.getTasks(), idTask);
+    // tdL.getTasks().forEach((element, index) => {
+    //   console.log(element.description, element.state, index);
+    // });
+  }
+});
+button.addEventListener('click', () => {
+  const taskR = tdL.getTasks().filter((task) => !task.state);
+  tdL.tasks = taskR;
+  localStorage.setItem('tasks', JSON.stringify(taskR));
+  clearSelected();
+  printTasks(tdL.getTasks());
 });
 
 input.addEventListener('keyup', (e) => {
   if (e.key === 'Enter') {
-    const tsk = new Task(e.target.value); // default false
+    const tsk = new Task(e.target.value, false); // default false
     tdL.addTask(tsk);
-    const tareas = tdL.getTasks();
+    // const tareas = tdL.getTasks();
     localStorage.setItem('tasks', JSON.stringify(tdL.getTasks()));
-    console.log(localStorage.getItem('tasks'));
-    console.log(`GetTask${tdL.getTasks().length}`);
-    tareas.forEach((element, index) => {
-      console.log(element.description, element.state, index);
-    });
+    // console.log(localStorage.getItem('tasks'));
+    // console.log(`GetTask${tdL.getTasks().length}`);
+    // tareas.forEach((element, index) => {
+    //   console.log(element.description, element.state, index);
+    // });
     clearAll.insertAdjacentElement('beforebegin', printTask(tsk, tdL.getTasks().length - 1));
     input.value = '';
   }
+  return true;
 });
 
 document.addEventListener('input', (e) => {
   const taskEdit = e.target;
+  if (!e.target.matches('.inTask')) { return false; }
+
+  const idTask = parseInt(taskEdit.name.slice(5), 10);
+  tdL.editTask(idTask, e.target.value);
+  localStorage.setItem('tasks', JSON.stringify(tdL.getTasks()));
+
+  return true;
+});
+
+document.addEventListener('click', (e) => {
+  const taskEdit = e.target;
+  const imgs = document.querySelectorAll('.container-svg img');
   if (e.target.matches('.inTask')) {
     const idTask = parseInt(taskEdit.name.slice(5), 10);
-    tdL.editTask(idTask, e.target.value);
-    localStorage.setItem('tasks', JSON.stringify(tdL.getTasks()));
-    taskEdit.parentElement.nextElementSibling.firstElementChild.setAttribute('alt', 'delete');
-    taskEdit.parentElement.nextElementSibling.firstElementChild.setAttribute('src', trash);
+    imgs.forEach((e, index) => {
+      if (index > 0) {
+        if (index - 1 === idTask) {
+          e.setAttribute('alt', 'delete');
+          e.setAttribute('src', trash);
+        } else {
+          e.setAttribute('alt', 'edit');
+          e.setAttribute('src', moreVert);
+        }
+      }
+    });
+  } else {
+    return false;
   }
+  return true;
 });
 
 document.addEventListener('change', (e) => {
@@ -136,5 +241,8 @@ document.addEventListener('change', (e) => {
   if (e.target.matches('.inTask')) {
     taskEdit.parentElement.nextElementSibling.firstElementChild.setAttribute('alt', 'edit');
     taskEdit.parentElement.nextElementSibling.firstElementChild.setAttribute('src', moreVert);
+  } else {
+    return false;
   }
+  return true;
 });
